ab9d7d005c87308d14d3b624fb569b85aefc4715
Harvey Mei <harvey.mei@msn.com>
2021-08-16 16:57:13 +0800

    Commit Message

--- currency.py
+++ currency.py
@@ -6,28 +6,64 @@
 # @IDE     : PyCharm
 # @GitHub  : https://github.com/harveymei/
 
-import matplotlib.pyplot as plt
+"""
+1，记录外币增减记录及金额
+2，统计每种外币当前合计金额并绘图
+3，
+"""
 
+import matplotlib.pyplot as plt
+import csv
+
+# 文件操作，读取存取记录，并计算各个货币的当前余额
+filename = "data.csv"
+with open(filename) as f_object:
+    reader = csv.reader(f_object)
+    header_row = next(reader)  # 读取文件头（首行）
+
+    usd_list, hkd_list, eur_list = [], [], []
+    for row in reader:
+        currency_code = row[1]
+        if currency_code == 'usd':
+            usd_list.append(float(row[-1]))
+        elif currency_code == 'hkd':
+            hkd_list.append(float(row[-1]))
+        else:
+            eur_list.append(float(row[-1]))
+
+    for i in usd_list:
+        total_usd = total_usd + usd_list[i]
+    for j in hkd_list:
+        total_hkd = total_hkd + hkd_list[j]
+    for k in range(0, len(eur_list)):
+        total_eur = total_eur + eur_list[k]
+
+        # 判断货币类型，根据判断结果分别取值并放入各个货币列表
+
+
+
+# 绘图
 # plt.rcdefaults()  # Restore the rcParams from Matplotlib's internal default style.
 fig, ax = plt.subplots()
 
 # Example data
-people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim', 'Jerry')
+currency_name = ('USD', 'HKD', 'EUR')
 """
 元组中为三种外币的合计金额，当增加或减少时（记录每次增减变化，并重新计算总和），总额应产生变化。
 """
-performance = (2.00, 4.00, 6.00, 8.00, 10.00)
+# performance = (2.00, 4.00, 6.00, 8.00, 10.00)
+total_amount = (total_usd, total_hkd, total_eur)  #
 
 # https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.barh.html#matplotlib.axes.Axes.barh
 # 位置参数y，条状图的y坐标值，浮点数或者数组形式。
 # 位置参数width，条状图宽度值，浮点数或者数组形式。
-ax.barh((0, 2, 3, 5, 8), performance, align='center')  # 使用元组传递x轴刻度值
+ax.barh((0, 1, 2), total_amount, align='center')
 
 # 设置y轴刻度位置，浮点数列表，列表中元素数量应与y轴刻度标签数量一致。
 # https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.set_yticks.html#matplotlib.axes.Axes.set_yticks
-ax.set_yticks([0, 2, 3, 5, 8, 10])
+ax.set_yticks([0, 1, 2])
 
-ax.set_yticklabels(people)  # 设置y轴刻度标签
+ax.set_yticklabels(currency_name)  # 设置y轴刻度标签
 ax.invert_yaxis()  # 标签从上到下阅读，即倒置y轴（改变排序）
 ax.set_xlabel('Performance')  # 设置x轴标签
 ax.set_title('How fast do you want to go today?')  # 设置图表标题
